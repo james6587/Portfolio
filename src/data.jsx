7ab701482c84d33e71690d4053810df70e742f92
James <jameseliotv@gmail.com>
2024-05-03 05:33:41 -0500

    update

--- src/data.jsx
+++ src/data.jsx
@@ -51,7 +51,7 @@ export const skills = [
     skill: "GIT version controller",
   },
   {
-    image: "/src/assets/icons/blender.png",
+    image: "/src/assets/blender.png",
     skill: "Blender",
   },
   {
